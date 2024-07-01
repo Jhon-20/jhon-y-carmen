757be7754c06140c3a7ae04f9ed113c563a4bbe0
Jhon <jasoplopuco@synopsis.onmicrosoft.com>
2024-06-30 23:58:57 -0500

    otros cambios

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -18,16 +18,15 @@ const Cart = () => {
             <section>
                 <div className="flex items-center flex-col md:flex-row justify-center gap-12 lg:gap-24  lg:py-0">
                     <div className='flex flex-col justify-center items-center'>
-                        <img className="w-[200px] md:w-[300px]" src={arco} alt="arco" />
-                        <div className='flex flex-col justify-center items-center gap-5 mb-5'>
-                            <h1 className='primaryFont text-3xl md:text-5xl text-[#464444]'>Jhon aaa y Carmen</h1>
-                            <p className='secondFont text-lg md:text-2xl  font-normal text-[#717171]'>12 de Enero | 04:00 pm</p>
+                        <img className="w-[220px] md:w-[300px]" src={arco} alt="arco" />
+                        <div className='flex flex-col justify-center items-center gap-7 mb-7'>
+                            <h1 className='primaryFont text-4xl md:text-5xl text-[#464444]'>Jhon y Carmen</h1>
+                            <p className='secondFont text-xl md:text-2xl  font-normal text-[#717171]'>12 de Enero | 04:00 pm</p>
                         </div>
-
-                        <img className="w-[200px] md:w-[300px]" src={arcoDown} alt="arco" />
+                        <img className="w-[220px] md:w-[300px]" src={arcoDown} alt="arco" />
                     </div>
                     <div className='flex flex-col justify-center items-center'>
-                        <img className='sobre w-[240px] md:w-[380px] cursor-pointer mb-6' src={sobre} alt="sobre" />
+                        <img className='sobre w-[280px] md:w-[380px] cursor-pointer mb-6' src={sobre} alt="sobre" />
                         <p className='text-[18px] text-[#717171] secondFont font-light mb-6'>LINKS DE AYUDA</p>
                         <div className='flex gap-10'>
                             <div className='flex flex-col items-center gap-2'>
