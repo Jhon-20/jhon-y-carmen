831ae6c67ef547fb5b50314fd3bd5ba140d89e28
Jhon <jasoplopuco@synopsis.onmicrosoft.com>
2024-07-09 15:03:54 -0500

    modal presentes

--- src/pages/components/chip/numero_chip.tsx
+++ src/pages/components/chip/numero_chip.tsx
@@ -1,10 +1,10 @@
-import { Chip } from "@mui/material"
+
 import { Copy, Heart } from "iconsax-react";
 interface NumeroChipProps {
   label: String;
   onDelete: () => void;
 }
-const NumeroChip: React.FC<NumeroChipProps> = ({ label, onDelete }) => {
+const NumeroChip: React.FC<NumeroChipProps> = ({ label }) => {
   return (
     <div className="bg-[#FBF1F2] rounded-lg w-full p-2 flex justify-between">
       <div className="flex gap-2">
@@ -13,15 +13,7 @@ const NumeroChip: React.FC<NumeroChipProps> = ({ label, onDelete }) => {
       </div>
       <Copy size="24" color="#545454" variant="Outline" className="cursor-pointer" />
     </div>
-    // <Chip
-    //   label={label}
-    //   icon={<Heart size="24" color="#AB7779" variant="Bold" />}
-    //   deleteIcon={<Copy size="24" color="#545454" variant="Outline" />}
-    //   onDelete={onDelete}
 
-    //   sx={{ fontSize: "16px", background: "#FBF1F2", fontFamily: "Montserrat" }}
-    //   className="secondFont text-[#545454] w-full flex justify-between"
-    // />
 
 
   )
