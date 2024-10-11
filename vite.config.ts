f6837057b594197b024787142ca4dd5dc3592c3f
Jhon <jasoplopuco@synopsis.onmicrosoft.com>
2024-10-11 17:48:37 -0500

    nuevos cambios

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Jhon-20.github.io/jhon-y-carmen'
+  base: 'https://Jhon-20.github.io/jhon-y-carmen',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Crea un chunk para las dependencias
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600,
+  },
 })
